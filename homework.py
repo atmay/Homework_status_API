d7b076b7581a05599a7cd98ad35c2de0fbb39602
atmay <liubov.burtseva@gmail.com>
2020-09-21 21:12:17 +0300

    after review version

--- homework.py
+++ homework.py
@@ -3,17 +3,22 @@ import requests
 import telegram
 import time
 from dotenv import load_dotenv
+from requests.exceptions import RequestException
 
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] != 'approved':
+    homework_status = homework.get('status')
+    homework_name = homework.get('homework_name')
+    if homework_status is None:
+        return 'Invalid server response'
+    elif homework_status != 'approved':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -21,15 +26,17 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     params = {'from_date': current_timestamp}
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    homework_statuses = requests.get(url, headers=headers, params=params)
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(url=url, headers=headers, params=params)
+        return homework_statuses.json()
+    except RequestException as error:
+        print(f'Error {error} occured while getting JSON')
 
 
 def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
